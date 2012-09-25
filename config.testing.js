da6a83f2a3967aa2bd4f650d452700d2c283e815
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2012-09-25 16:36:38 +0200

    Nodeunit testing code: currently, only for roq-connector.

---
+++ config.testing.js
@@ -0,0 +1,7 @@
+module.exports =[
+    './plugins/roq-connector',
+    //'./plugins/roq-web-api',
+    //'./plugins/roq-cloudmanager',
+    //'./plugins/roq-orchestrator',
+    //'./plugins/roq-testing',
+]
--- roq-connector.test.js
+++
@@ -1,6 +0,0 @@
-
-exports.testConnecting = function(test){
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
-};
