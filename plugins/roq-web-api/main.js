dd929151792ff961cb48d4f63c46f1597cc14741
Jehan Bruggeman <jehan.bruggeman@euranova.eu>
2012-10-09 15:33:02 +0200

    pagination and sorting. Closes #4.

--- plugins/roq-web-api/main.js
+++ plugins/roq-web-api/main.js
@@ -43,18 +43,71 @@ module.exports = function setup(options, imports, register) {
          app.listen(port);
     }
     
-    var formatList = function(list){
-        if(!list)
-            return {"success":false};
-        else
-        return {
-                    "success" : true,
-                    "results": list.length,
-                    "rows": list
-                };
+    var isDefined = function(v){
+		return (v != undefined) && !isNaN(v);
+	}
+    
+    var sortList = function(list,sort){
+		if(null == sort.property || null == sort.direction){
+			log.trace("invalid sort object");
+		}else{
+			log.trace("valid sort object");
+			var dir=1;
+			if("ASC" == sort.direction)
+				dir = -1;
+			list.sort(function(a,b){
+				log.trace("sort:",a,b);
+				var ap = a[sort.property];
+				var bp = b[sort.property];
+				if(null != ap && null != bp){
+					if(ap.toUpperCase() == bp.toUpperCase())
+						return 0
+					return dir*((ap.toUpperCase() < bp.toUpperCase())*2-1);
+				}else if(null != ap){
+					return dir;
+				}else if(null != bp){
+					return -1*dir;
+				}else{
+					return 0;
+				}
+			});
+		}
+		return list;
+	}
+    
+    var formatList = function(list,page,start,limit,sort){
+		var list;
+		
+		if(!list)
+			return {"success":false};
+			
+		// if necessary, sort the results
+		if(undefined != sort){
+			sort = JSON.parse(sort);
+			log.trace("sort ", sort);
+			for(var i in sort){
+				sortList(list,sort[i]);
+			}
+		}	
+		// if we don't have start, try using page to deduce start
+		if( !isDefined(start) && isDefined(page) && isDefined(limit)){
+			start = (page-1)*limit;
+		}
+		
+		// slice if it makes sense
+		if(isDefined(start) && isDefined(limit) && start >=0 && limit >= 0){
+			log.trace("return results "+start+" to "+(start+limit-1));
+			list = list.slice(start,start+limit);
+		}
+		
+		return {
+				"success" : true,
+				"results": list.length,
+				"rows": list
+			};
     }
     
-    var getDefaultCallback = function(res,req){
+    var getDefaultCallback = function(req,res){
         return function(error){
             if(null == error)
                 res.send({success:true});
@@ -67,7 +120,12 @@ module.exports = function setup(options, imports, register) {
         app.namespace('/hosts',function(){
             app.get('/list',function(req,res){
                 log.trace('list hosts');
-                res.send(formatList(controller.listHosts()));
+                res.send(
+					formatList(controller.listHosts(),
+						parseInt(req.query.page),
+						parseInt(req.query.start),
+						parseInt(req.query.limit),
+						req.query.sort));
             });
         });
         
@@ -75,7 +133,11 @@ module.exports = function setup(options, imports, register) {
             
             app.get('/list',function(req,res){
                 log.trace('list queues');
-                res.send(formatList(controller.listQueues()));
+                res.send(formatList(controller.listQueues(),
+						parseInt(req.query.page),
+						parseInt(req.query.start),
+						parseInt(req.query.limit),
+						req.query.sort));
             });
             
             app.namespace('/:id',function(){
